1f9f8782c6efa2ab078cb01a8b6250a2004d7b02
karebetconnec <karebetconnec@gmail.com>
2024-09-26 04:34:10 +0000

    json

---
+++ robeth.my.id-json.js
@@ -0,0 +1,326 @@
+{
+    "title" :"Achmed Robeth Muzaki | robeth.my.id",
+    "main_position" :"Fullstack Developer & Head of IT",
+    "core_position" :"Fullstack Developer",
+    "text":"<h1 class='h1 fw-bold'>👋 Hi, <span class='fw-light'>i`m</span> Robeth Muzaki</h1> <p class='h3'>Fullstack Developer &amp; Head of IT <br>di <a class='text-dark link-underline-dark' href='https://iziroam.com'>iziRoam</a>.</p> <p>Saya adalah seorang Programmer, yang sudah 13 tahun mendalami bidang Teknologi. </p> <p>Sering membuat Aplikasi / Web:</p> <ul> <li>CMMS, GIS &amp; ERP</li> <li>E-Commerce , Company Profile &amp; Toko Online</li> <li>Mobile Applications &amp; Game</li> </ul> <p> Jika memiliki Project, dengan senang hati saya akan membantu untuk menyelesaikannya. <br>  </p>",
+    "link_sosmed" : [
+        {"icon":"wa","name":"Whatsapps","link":"https://wa.me/6282170699887"},
+        {"icon":"email","name":"Email","link":"mailto:ach.robeth@gmail.com"},
+        {"icon":"github","name":"Github","link":"https://github.com/karebet"},
+        {"icon":"linkedin","name":"LinkedIn","link":"https://www.linkedin.com/in/achmed-robeth-muzaki"}
+    ],
+    "timeline":[
+        {
+            "tahun": "2","tahun_end": "tahun","sebagai": "Head of IT & Fullstack Developer","time": "2022 - sekarang","hitung_tahun":2022,
+            "textdesc": "Membangun dan maintenance web iziroam.com & mobile apps iziRoam. Memanagement dan melakukan riset dan analisis, perencanaan, dan desain terhadap kebutuhan setiap sistem dan aplikasi",
+            "location": "PT Solusi Untuk Semua"
+        },
+        {
+            "tahun": "6","tahun_end": "tahun","sebagai": "Fullstack Developer","time": "2016 - 2022","hitung_tahun":0,
+            "textdesc": "Mengembangkan fitur: Rental mobil, Jual beli Mobil, Jual Asuransi Mobil, Dashboard Penjual. Membangun dan maintenance mobiloka.com , iziroam.com, izihealth.com dan lain-lain.",
+            "location": "PT Mobiloka Mitra Auto"
+        },
+        {
+            "tahun": "1","tahun_end": "tahun","sebagai": "Fullstack Developer","time": "2015 - 2016","hitung_tahun":0,
+            "textdesc": "Membangun web dan aplikasi, e-Commerce, ERP, CMMS & Portofolio Perusahaan dan anak Perusahaan. Menganalisa dan mengimplementasikan kebutuhan Client untuk membuat web.",
+            "location": "PT Togu Inovasi Teknologi (Lobunta Group)"
+        },
+        {
+            "tahun": "4","tahun_end": "tahun","sebagai": "Mahasiswa","time": "2011 - 2015","hitung_tahun":0,
+            "textdesc": "Teknik Informatika, Skripsi & Jurnal : APLIKASI PENGAMAN DATA DAN INFORMASI BERLAPIS DENGAN METODE STEGANOGRAFI LSB, KRIPTOGRAFI OPENSSL DAN MD5 BERBASIS WEB <a target=\"_blank\" class=\"text-dark small\" href=\"https://ojs.amikom.ac.id/index.php/semnasteknomedia/article/view/1014\">(Baca Abstract)</a>",
+            "location": "Universitas AMIKOM Yogyakarta"
+        }
+    ],
+    "skill":{
+        "PHP":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logophp.svg",
+        "Dart":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logodart.svg",
+        "ASP.NET":"https://www.simplilearn.com/ice9/free_resources_article_thumb/ASP.NET_logo.jpg",
+        "Codeigniter":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoci.svg",
+        "Laravel":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logolaravel.svg",
+        "NextJS":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240808-logonext.svg",
+        "Flutter":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoflutter.svg",
+        "NodeJS":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logonodejs.svg",
+        "SQLServer":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logomssql.svg",
+        "MongoDB":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logomongodb.svg",
+        "AndEngine":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoandengine.png",
+        "AdobeIllustrator":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoai.svg",
+        "Premier Pro":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logopr.svg",
+        "Figma":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logofigma.svg",
+        "SyS Admin":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logosysadmin.svg",
+        "Linux":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logolinux.svg",
+        "Github":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240808-logogithub.svg",
+        "AWS":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoaws.svg",
+        "Google Cloud":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logogooglecloud.svg",
+        "Alibaba Cloud":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logoalibaba.svg"
+    },
+    "portfolio":[
+            {
+                "title": "UMKM JadiPro","subtitle": "Web Development",
+                "bg": "bg-white","tx": "text-primary",
+                "time": "Jun 2024","location": "Jakarta,ID",
+                "url": "https://jadipro.id/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/jadpro-svg-mini-png.svg"
+            },
+            {
+                "title": "PT Berkat Hijau Makmur Abadi (BHMA)","subtitle": "Web Development",
+                "bg": "bg-white","tx": "text-success",
+                "time": "Feb 2024","location": "Jakarta,ID",
+                "url": "https://berkathijau.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/bhma.jpg"
+            },
+            {
+                "title": "zRoam","subtitle": "Web Development",
+                "bg": "bg-white","tx": "text-primary",
+                "time": "Oct 2023","location": "Jakarta,ID",
+                "url": "https://zroam.robeth.my.id/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/zroam-base64img.svg"
+            },
+            {
+                "title": "Little Frenchie","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-danger",
+                "time": "Sept 2023", "location": "Bali,ID",
+                "url": "https://littlefrenchie.id",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/lf.jpg"
+            },
+            {
+                "title": "kasirResto - ASP.NET","subtitle": "Web Development",
+                "bg": "bg-dark","tx": "text-white",
+                "time": "Oct 2022","location": "Jakarta,ID",
+                "url": "https://github.com/karebet/aspdotnet_kasirResto",
+                "img": "https://raw.githubusercontent.com/karebet/aspdotnet_kasirResto/main/screenshot.png"
+            },
+            {
+                "title": "UMKM.Online","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-success",
+                "time": "Sep 2022","location": "Jakarta,ID",
+                "url": "https://umkm.online",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/umkm.online.png"
+            },
+            {
+                "title": "iziRoam - Apps", "subtitle": "Mobile Apps",
+                "bg": "bg-izi","tx": "text-white",
+                "time": "Apr 2022","location": "Jakarta,ID",
+                "url": "https://play.google.com/store/apps/details?id=com.izi.iziroam",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/izir3.jpg"
+            },
+            {
+                "title": "Whatsapp Bot","subtitle": "Whatsapp Bot untuk mendukung proyek pembangunan BTS",
+                "bg": "bg-wa","tx": "text-white",
+                "time": "Jan 2022","location": "Jakarta,ID",
+                "url": "https://github.com/karebet/wa_api_bot",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/WAB_1.gif"
+            },
+            {
+                "title": "Laravel - Parkiro","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-danger",
+                "time": "Sept 2021","location": "Jakarta,ID",
+                "url": "https://parkiro-laravel-robeth-my-id.go.yj.fr/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/noimg.jpg"
+            },
+            {
+                "title": "Rasa Bakery and Cafe","subtitle": "Web Development",
+                "bg": "bg-rasabakery","tx": "text-white",
+                "time": "Sept 2021","location": "Bandung,ID",
+                "url": "https://rasabakeryandcafe.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/rsb.jpg"
+            },
+            {
+                "title": "iziHealth - Konsultasi Dokter Online","subtitle": "Web Development",
+                "bg": "bg-izi", "tx": "text-white",
+                "time": "Sept 2021","location": "Bandung,ID",
+                "url": "https://play.google.com/store/apps/dev?id=6053576014965730914",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/izih1.jpg"
+            },
+            {
+                "title": "iziRoam - Rental WiFi", "subtitle": "Web Development",
+                "bg": "bg-izi","tx": "text-white",
+                "time": "Nov 2019","location": "Jakarta,ID",
+                "url": "https://iziroam.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/izir2.jpg"
+            },
+            {
+                "title": "Oma Elly", "subtitle": "Web Development",
+                "bg": "bg-omaelly","tx": "text-white",
+                "time": "Apr 2019","location": "Jakarta,ID",
+                "url": "https://omaelly.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/oma.jpg"
+            },
+            {
+                "title": "SCNELLO - Tailor Online","subtitle": "Web Development",
+                "bg": "bg-scenello","tx": "text-white",
+                "time": "Sep 2018","location": "Jakarta,ID",
+                "url": "https://scnello.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/sc.1.png"
+            },
+            {
+                "title": "Aplikasi Kalkulator", "subtitle": "Mobile Apps",
+                "bg": "bg-izi", "tx": "text-white",
+                "time": "Jul 2018","location": "Jakarta,ID",
+                "url": "https://github.com/karebet/react-native-example-calculator/blob/master/Kalkulator.apk?raw=true",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/kk.jpg"
+            },
+            {
+                "title": "Eastern Merchant","subtitle": "Web Development",
+                "bg": "bg-eastern","tx": "text-white",
+                "time": "Mei 2018","location": "Jakarta,ID",
+                "url": "https://theeasternfoods.com",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ea.1.jpg"
+            },
+            {
+                "title": "Eastern Merchant","subtitle": "Web Development, System Inventory Warehouse",
+                "bg": "bg-eastern","tx": "text-white",
+                "time": "Mei 2018","location": "Jakarta,ID",
+                "url": "https://theeasternfoods.com",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ea.2.jpg"
+            },
+            {
+                "title": "Gadogado","subtitle": "Web Development",
+                "bg": "bg-elhart","tx": "text-white",
+                "time": "Jan 2018","location": "Jakarta,ID",
+                "url": "https://gadogado.id/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/gdgd1.jpg"
+            },
+            {
+                "title": "iziRoam - Rental WiFi","subtitle": "Web Development",
+                "bg": "bg-izi","tx": "text-white",
+                "time": "Sep 2017",
+                "location": "Jakarta,ID",
+                "url": "https://iziroam.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/iz.1.jpg"
+            },
+            {
+                "title": "Mobiloka.com","subtitle": "Web Development",
+                "bg": "bg-izi","tx": "text-white",
+                "time": "Jan 2017","location": "Jakarta,ID",
+                "url": "https://mobiloka.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/mb.1.jpg"
+            },
+            {
+                "title": "Resepps","subtitle": "Web Development",
+                "bg": "bg-secondary","tx": "text-white",
+                "time": "Mei 2016","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/rp.jpg"
+            },
+            {
+                "title": "Trumecs.com","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-dark",
+                "time": "Des 2016","location": "Jakarta,ID",
+                "url": "https://trumecs.com",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/tr.jpg"
+            },
+            {
+                "title": "Lobunta Land","subtitle": "Web Development",
+                "bg": "bg-lobuntaland", "tx": "text-white",
+                "time": "Sep 2016","location": "Jakarta,ID",
+                "url": "https://lobuntaland.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ll.1.jpg"
+            },
+            {
+                "title": "Lobunta Lubricants","subtitle": "Web Development",
+                "bg": "bg-lobuntalubricant","tx": "text-white",
+                "time": "Jun 2016","location": "Jakarta,ID",
+                "url": "https://www.lobuntalubricants.com/",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/lls.1.jpg"
+            },
+            {
+                "title": "Logvest Agro Makmur","subtitle": "Web Development",
+                "bg": "bg-lobuntaland","tx": "text-white",
+                "time": "Mar 2016","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/lo.1.jpg"
+            },
+            {
+                "title": "PT.Elhart Makmur Persada","subtitle": "Web Development",
+                "bg": "bg-elhart","tx": "text-white",
+                "time": "Jan 2016","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/noimg.jpg"
+            },
+            {
+                "title": "Seek Shelter","subtitle": "Game Development",
+                "bg": "bg-gamess","tx": "text-white",
+                "time": "Des 2015","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ss.jpg"
+            },
+            {
+                "title": "PT. Lobunta Mandiri Persada","subtitle": "Web Development",
+                "bg": "bg-e16416","tx": "text-white",
+                "time": "Agu 2015","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/lpm.1.jpg"
+            },
+            {
+                "title": "Togu Parkir","subtitle": "Apps Development",
+                "bg": "bg-e16416","tx": "text-white",
+                "time": "Jun 2015","location": "Jakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/tp.jpg"
+            },
+            {
+                "title": "Steganografi","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-dark",
+                "time": "Feb 2015","location": "Yogyakarta,ID",
+                "url": "https://ojs.amikom.ac.id/index.php/semnasteknomedia/article/view/1014",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/sf.jpg"
+            },
+            {
+                "title": "Ombie Base Game","subtitle": "Game Development",
+                "bg": "bg-dark","tx": "text-white",
+                "time": "Nov 2014","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/om.jpg"
+            },
+            {
+                "title": "Fuleco Maskot Piala Dunia","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-success",
+                "time": "Jun 2014","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/fl.jpg"
+            },
+            {
+                "title": "Jump Around","subtitle": "Game Development",
+                "bg": "bg-2b3772","tx": "text-white",
+                "time": "Jan 2014","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ja.jpg"
+            },
+            {
+                "title": "Game Save Gaza","subtitle": "Game Development",
+                "bg": "bg-dark","tx": "text-danger",
+                "time": "Jul 2013","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/sg.jpg"
+            },
+            {
+                "title": "Game Racing Old","subtitle": "Game Development",
+                "bg": "bg-dark","tx": "text-success",
+                "time": "Jan 2013","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/rc.jpg"
+            },
+            {
+                "title": "Angklung Desktop","subtitle": "Desktop Development",
+                "bg": "bg-1a180c","tx": "text-c5904c",
+                "time": "Des 2012","location": "Yogyakarta,ID",
+                "url": "https://www.youtube.com/watch?v=yyxN6RU0ZfA",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/ang.jpg"
+            },
+            {
+                "title": "Gigeti Resto","subtitle": "Web Development",
+                "bg": "bg-light","tx": "text-c5904c",
+                "time": "Agu 2012","location": "Yogyakarta,ID",
+                "url": "#",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/gg.jpg"
+            },
+            {
+                "title": "Terminal Code Web","subtitle": "Web Development",
+                "bg": "bg-dark","tx": "text-danger",
+                "time": "Mei 2012","location": "Yogyakarta,ID",
+                "url": "https://www.youtube.com/watch?v=8NkInRKcOz8",
+                "img": "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/tc.jpg"
+            }
+    ],
+    "nickname": "Robeth"
+}
