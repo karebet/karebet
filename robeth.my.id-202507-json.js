5c0b638f2a05449c1ead235aad27d5972df528aa
Robeth Muzaki <karebetconnec@gmail.com>
2025-07-16 18:18:45 +0700

    Update and rename robeth.my.id-202505-json.js to robeth.my.id-202507-json.js

--- robeth.my.id-202507-json.js
+++ robeth.my.id-202507-json.js
@@ -5,9 +5,9 @@
     "nick_name" :"Robeth",
     "image" : "https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/RBT.png",
     "logo":"https://umkmku.s3.ap-southeast-1.amazonaws.com/img/20240306-logorobeth.png",
-    "main_position" :"Fullstack Developer & IT Dev Team Lead",
+    "main_position" :"Fullstack Developer & Head of IT Software & Network",
     "core_position" :"Fullstack Developer",
-    "text":"<h1 class='h1 fw-bold'>👋 Hi, <span class='fw-light'>i'm</span> Robeth Muzaki</h1> <p class='h3'>Fullstack Developer &amp; IT Dev Team Lead di <a class='text-dark link-underline-dark' href='https://berkathijaugroup.com/'>Berkat Hijau Group</a>.</p> <p>Saya adalah seorang Programmer, yang sudah 13 tahun mendalami bidang Teknologi. </p> <p>Sering membuat Aplikasi / Web:</p> <ul> <li>CMMS, GIS &amp; ERP</li> <li>E-Commerce , Company Profile &amp; Toko Online</li> <li>Mobile Applications &amp; Game</li> </ul> <p> Jika memiliki Project, dengan senang hati saya akan membantu untuk menyelesaikannya. </p>",
+    "text":"<h1 class='h1 fw-bold'>👋 Hi, <span class='fw-light'>i'm</span> Robeth Muzaki</h1> <p class='h3'>Fullstack Developer &amp; Head of IT Software & Network di <a class='text-dark link-underline-dark' href='https://berkathijaugroup.com/'>Berkat Hijau Group</a>.</p> <p>Saya adalah seorang Programmer, yang sudah 13 tahun mendalami bidang Teknologi. </p> <p>Sering membuat Aplikasi / Web:</p> <ul> <li>CMMS, GIS &amp; ERP</li> <li>E-Commerce , Company Profile &amp; Toko Online</li> <li>Mobile Applications &amp; Game</li> </ul> <p> Jika memiliki Project, dengan senang hati saya akan membantu untuk menyelesaikannya. </p>",
     "customehtml1":"<div class='row mb-5'> <div class='col-md-12'> <a target='_blank'  rel='noreferrer' href='https://jadipro.id' class='text-decoration-none card card-body px-5 rounded-5 pb-0 shadow border-0 bg-primary'> <div class='row align-items-center'> <div class='col-md-9'> <h2 class='h1'>UMKM punya website gratis!</h2> </div> <div class='col-md-3'> <img class='w-100' src='https://umkmku.s3.ap-southeast-1.amazonaws.com/rbtimg/jadpro-svg-mini-png.svg' alt='umkm jadipro'> </div> </div> </a> </div> </div>",
     "link_sosmed" : [
         {"icon":"wa","name":"Whatsapps","link":"https://wa.me/6282170699887"},
@@ -17,7 +17,7 @@
     ],
     "timeline":[
         {
-            "tahun": "2","tahun_end": "tahun","sebagai": "IT Dev Team Lead & Fullstack Developer","time": "2025 - sekarang","hitung_tahun":2025,
+            "tahun": "2","tahun_end": "tahun","sebagai": "Head of IT Software & Network","time": "2025 - sekarang","hitung_tahun":2024,
             "textdesc": "Membangun dan maintenance system internal. Memanagement dan melakukan riset dan analisis, perencanaan, dan desain terhadap kebutuhan setiap sistem dan aplikasi",
             "location": "PT Berkat Hijau Makmur Abadi (Berkat Hijau Group)"
         },
